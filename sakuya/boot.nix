e38cc0e08d7be3cbb9441b758cd0b1dc18557444
dramforever <dramforever@live.com>
2021-06-02 20:08:27 +0800

    Fix typo

--- sakuya/boot.nix
+++ sakuya/boot.nix
@@ -9,7 +9,7 @@
 
     kernelParams = [ "quiet" "mitigations=off" "mem_sleep_default=deep" ];
 
-    kernelPackages = linuxPackages_5_12;
+    kernelPackages = pkgs.linuxPackages_5_12;
     # extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
 
     supportedFilesystems = [ "ntfs" "exfat" ];
