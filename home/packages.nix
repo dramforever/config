704970a6c4ab1e0a3ab388ab7e986887fcffba3e
dramforever <dramforever@live.com>
2022-01-28 20:45:12 +0800

    Cleanup

--- home/packages.nix
+++ home/packages.nix
@@ -5,77 +5,45 @@
     ghc
     cabal-install
     ark
-    audacity
     bat
     bind
     binutils
     chromium
-    cloc
-    coq_8_11
-    direnv
-    dotnet-sdk
-    ffmpeg-full
     file
     gdb
-    # git
     gtkwave
     gwenview
     hexchat
     htop
-    # hmcl
     inkscape
     jdk11
-    jetbrains.idea-community
     jq
     kdeconnect
-    keepassxc
-    kgpg
     kicad
     ksshaskpass
     libarchive
-    lrzsz
-    # mathematica
-    mono
-    # niv
-    # nix-direnv
     nix-index
     nodejs
     obs-studio
     okular
-    # onlyoffice
-    # osu-lazer
-    patchelf
     pinentry-qt
     pinta
     plasma-browser-integration
     plasma5Packages.bismuth
     python3
-    # quartus-prime-lite
     ripgrep
-    socat
     spectacle
-    sqliteInteractive
     stack
     steam
     syncthing
     syncthingtray
-    # tang-dynasty
     tdesktop
     thunderbird
     tig
-    # tmux
-    # typora
-    universal-ctags
-    unrar
     usbutils
-    verilog
     vlc
     vscode
-    wireshark
     wolfram-engine
-    # zhumu
     zoom-us
-    # zsh-completions
-    # zsh-autosuggestions
   ];
 }
