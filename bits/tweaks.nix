d19647d18146ce478afaa0c0000b4ed8ace197cc
dramforever <dramforever@live.com>
2022-01-31 23:47:42 +0800

    Workaround for pam bug thing
    https://github.com/NixOS/nixpkgs/pull/156974

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -8,4 +8,16 @@ self: super:
   obs-studio = super.obs-studio.override {
     ffmpeg = self.ffmpeg-full;
   };
+  
+  plasma5Packages = super.plasma5Packages.overrideScope' (kself: ksuper: {
+    kscreenlocker = ksuper.kscreenlocker.override {
+      pam = self.pam-fixed;
+    };
+
+    plasma5 = ksuper.plasma5.overrideScope' (kpself: kpsuper: {
+      kscreenlocker = ksuper.kscreenlocker.override {
+        pam = self.pam-fixed;
+      };
+    });
+  });
 }
