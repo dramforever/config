5d1618c0d0955063e4e39e888156ecd61068c162
dramforever <dramforever@live.com>
2022-02-20 10:07:32 +0800

    Add Gandi DDNS

--- madoka/configuration.nix
+++ madoka/configuration.nix
@@ -16,6 +16,7 @@
     secrets = {
       hostapd_conf = {};
       goauthing = {};
+      gandi_auth_header = {};
     };
   };
 
@@ -132,6 +133,23 @@
   systemd.timers.goauthing = {
     timerConfig = {
       OnCalendar = "minutely";
+    };
+    wantedBy = [ "timers.target" ];
+  };
+
+  systemd.services.gandi-ddns = {
+    serviceConfig = {
+      Type = "oneshot";
+      DynamicUser = true;
+      LoadCredential = "gandi_auth_header:${config.sops.secrets.gandi_auth_header.path}";
+    };
+    path = with pkgs; [ iproute2 jq curl ];
+    script = builtins.readFile ./gandi.sh;
+  };
+
+  systemd.timers.gandi-ddns = {
+    timerConfig = {
+      OnCalendar = "hourly";
     };
     wantedBy = [ "timers.target" ];
   };
---
+++ madoka/gandi.sh
@@ -0,0 +1,21 @@
+set -eo pipefail
+
+ip --json addr show eth0 \
+    | jq '{
+            "items": [
+                [ .[].addr_info[] | select(.scope == "global") ]
+                | group_by(.family)[]
+                | {
+                    rrset_type: { "inet": "A", "inet6": "AAAA" }[.[0].family],
+                    rrset_values: [ .[].local ]
+                }
+            ]
+        }' \
+    | curl -sS -X PUT -d @- \
+        -H @"$CREDENTIALS_DIRECTORY/gandi_auth_header" \
+        -H 'Content-Type: application/json' \
+        https://api.gandi.net/v5/livedns/domains/dram.page/records/madoka \
+    | jq 'if .status == "error"
+            then "API Error: \(.)" | halt_error(1)
+            else "Success: \(.)" | halt_error(0)
+        end'
