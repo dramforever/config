eae835ca79f58553d12434f959a610cf5fc3d1f2
dramforever <dramforever@live.com>
2022-03-16 11:39:55 +0800

    noatime

--- sakuya/filesystems.nix
+++ sakuya/filesystems.nix
@@ -8,7 +8,7 @@ let
   subvolume = name: {
     device = btrfsDev;
     fsType = "btrfs";
-    options = [ "subvol=/${name}" "compress-force=zstd" ];
+    options = [ "subvol=/${name}" "compress-force=zstd" "noatime" ];
   };
 
 in {
