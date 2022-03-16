6244efa8e4bb5365872170974b58ebc1a2b043b5
dramforever <dramforever@live.com>
2022-03-16 11:42:55 +0800

    Update deps

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -6,12 +6,4 @@ self: super:
       nvenc = true;
     };
   };
-
-  # https://github.com/NixOS/nixpkgs/pull/162568 
-
-  thermald =
-    assert ! builtins.elem "--disable-werror" super.thermald.configureFlags;
-    super.thermald.overrideAttrs (old: {
-      configureFlags = old.configureFlags ++ [ "--disable-werror" ];
-    });
 }
