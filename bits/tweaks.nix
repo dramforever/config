76c24b84ae5c6c3d0b5d3e908404f695c6eb4e03
dramforever <dramforever@live.com>
2022-03-17 23:46:14 +0800

    bind: Patch around build failure
    See https://github.com/NixOS/nixpkgs/pull/163854/files

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -6,4 +6,19 @@ self: super:
       nvenc = true;
     };
   };
+
+  bind =
+    if self.system != "aarch64-linux"
+    then super.bind
+    else
+      # https://github.com/NixOS/nixpkgs/pull/163854
+      assert builtins.compareVersions super.bind.version "9.18.1" < 0;
+      super.bind.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          (self.fetchpatch {
+            url = "https://gitlab.isc.org/isc-projects/bind9/-/commit/b465b29eaf5ad8b8882debff1f993b8288617f22.patch";
+            sha256 = "sha256-Vz861YuVbcH1hZN4ojNoZaDbWi/ZjOb/73wbeeYQHmo=";
+          })
+        ];
+      });
 }
