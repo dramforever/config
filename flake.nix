7bc92d43e0c904bbd438c3e80d4a3b9835da5305
dramforever <dramforever@live.com>
2022-07-25 21:46:37 +0800

    Unpin telegram

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Personal configuration and packages, by dramforever";
 
-  inputs.nixpkgs-telegram.url = "github:NixOS/nixpkgs/48037fd90426e44e4bf03e6479e88a11453b9b66";
   inputs.nixpkgs.url = "github:tuna-nixpkgs/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -21,7 +20,7 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-telegram, flake-utils, nix-dram, home-manager, sops-nix }:
+  outputs = { self, nixpkgs, flake-utils, nix-dram, home-manager, sops-nix }:
     flake-utils.lib.eachDefaultSystem (system: {
       legacyPackages = import nixpkgs {
         inherit system;
@@ -44,9 +43,6 @@
       overlays = {
         packages = (final: prev: import ./bits/packages.nix final prev);
         tweaks = (final: prev: import ./bits/tweaks.nix final prev);
-        replace-telegram = final: prev: {
-          tdesktop = nixpkgs-telegram.legacyPackages.${final.system}.tdesktop;
-        };
       };
 
       nixosConfigurations.sakuya = nixpkgs.lib.nixosSystem {
