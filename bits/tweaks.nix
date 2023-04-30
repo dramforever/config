dc6507540a7632d7c0edd21c7bb81f3a517180da
dramforever <dramforever@live.com>
2023-04-30 11:33:53 +0800

    Update nix-dram

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -1,10 +1,6 @@
 self: super:
 
 {
-  nix-dram = super.make-nix-dram {
-    nix = self.nixVersions.nix_2_14;
-  };
-
   obs-studio = super.obs-studio.override {
     ffmpeg_4 = super.ffmpeg-full.override {
       withNvenc = true;
