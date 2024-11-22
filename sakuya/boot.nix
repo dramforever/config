737428ffc0cfc74a604d11967cc1be85e39ea022
dramforever <dramforever@live.com>
2024-11-22 15:03:54 +0800

    sakuya: Enable zswap

--- sakuya/boot.nix
+++ sakuya/boot.nix
@@ -23,5 +23,10 @@
     binfmt.emulatedSystems = [ "aarch64-linux" ];
   };
 
+  systemd.tmpfiles.settings."zswap" = {
+    "/sys/module/zswap/parameters/enabled"."w-".argument = "1";
+    "/sys/module/zswap/parameters/zpool"."w-".argument = "zsmalloc";
+  };
+
   hardware.cpu.intel.updateMicrocode = true;
 }
