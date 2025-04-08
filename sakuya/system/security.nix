2f3b20aa9c95d648dbec40b6a5ea8789348deedc
dramforever <dramforever@live.com>
2025-04-08 14:52:29 +0800

    Enable sudo

--- sakuya/system/security.nix
+++ sakuya/system/security.nix
@@ -1,23 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  security.doas = {
-    enable = true;
-    extraRules = lib.mkForce [
-      {
-        groups = [ "wheel" ];
-        noPass = false;
-        persist = true;
-      }
-    ];
-  };
-
-  security.sudo.enable = false;
-
   security.pam = {
     u2f.control = "sufficient";
 
     services.sudo.u2fAuth = true;
-    services.doas.u2fAuth = true;
   };
 }
