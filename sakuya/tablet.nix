b7ccb688829745b2e993153846f69b28865e3752
dramforever <dramforever@live.com>
2025-04-10 16:10:37 +0800

    sakuya: tablet: Add a patched hid-uclogic instead

--- sakuya/tablet.nix
+++ sakuya/tablet.nix
@@ -3,11 +3,15 @@
 {
   services.xserver.wacom.enable = true;
 
-  services.udev.packages = [
-    pkgs.huion-switcher
-    (pkgs.make-udev-hid-bpf {
-      name = "0010-Gaomon__M7";
-      src = ./tablet/0010-Gaomon__M7.bpf.c;
-    })
+  # services.udev.packages = [
+  #   pkgs.huion-switcher
+  #   (pkgs.make-udev-hid-bpf {
+  #     name = "0010-Gaomon__M7";
+  #     src = ./tablet/0010-Gaomon__M7.bpf.c;
+  #   })
+  # ];
+
+  boot.extraModulePackages = [
+    (config.boot.kernelPackages.callPackage ./tablet/hid-uclogic-dram.nix {})
   ];
 }
