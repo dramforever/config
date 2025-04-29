fd3bafd4787f890bdf4765391a2aac7ea1e2c605
dramforever <dramforever@live.com>
2025-04-29 22:09:35 +0800

    Bump krohnkite

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -13,4 +13,18 @@ self: super:
       "-DHIMBAECHEL_GOWIN_DEVICES=all"
     ];
   });
+
+  kdePackages = super.kdePackages.overrideScope (kfinal: kprev: {
+    krohnkite =
+      assert self.lib.versionAtLeast "0.9.9.0" kprev.krohnkite.version;
+      kprev.krohnkite.overrideAttrs (old: {
+        version = "0.9.9.0-unstable-2025-04-28";
+        src = self.fetchFromGitHub {
+          owner = "anametologin";
+          repo = "krohnkite";
+          rev = "879810aa30baee8490e488cbfd0737957c69f745";
+          hash = "sha256-Famg/g+Qwux4dZa6+CMKP6dDHNHNvJDKTsWQDukHHGk=";
+        };
+      });
+  });
 }
