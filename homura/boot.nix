f78909ed36c4dd278b76ea2ee8e661b8fc24adaa
dramforever <dramforever@live.com>
2025-06-18 20:16:55 +0800

    homura: Add HID-BPF

--- homura/boot.nix
+++ homura/boot.nix
@@ -17,6 +17,23 @@
       "vm.vfs_cache_pressure" = 50;
       "kernel.sysrq" = 1;
     };
+
+    kernelPatches = [
+      {
+        name = "hid-btf";
+        patch = null;
+        extraConfig = ''
+          DEBUG_INFO y
+          DEBUG_INFO_DWARF5 y
+          DEBUG_INFO_BTF y
+          DEBUG_INFO_BTF_MODULES y
+          FTRACE y
+          FUNCTION_TRACER y
+          DYNAMIC_FTRACE y
+          HID_BPF y
+        '';
+      }
+    ];
   };
 
   systemd.tmpfiles.settings."zswap" = {
