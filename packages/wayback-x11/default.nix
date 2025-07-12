3f357fa1abd2fe427ee9ec21225ee7c51314f8fc
dramforever <dramforever@live.com>
2025-07-12 14:33:10 +0800

    wayback-x11: 0-unstable-2025-07-11 -> 0-unstable-2025-07-12

--- packages/wayback-x11/default.nix
+++ packages/wayback-x11/default.nix
@@ -18,14 +18,18 @@
 
 stdenv.mkDerivation {
   pname = "wayback";
-  version = "0-unstable-2025-07-11";
+  version = "0-unstable-2025-07-12";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "wayback";
     repo = "wayback";
-    rev = "8bc189f533bf2c62656ebb482441eed9464160db";
-    hash = "sha256-jyifQ6yXf1g6S9SFfJfvswEJwX8+kTqhj6H2j4I1Gkk=";
+    rev = "7bea670bd0753c2a9b3617f3a9670dda1fa55ccc";
+
+/* FIXME: Upstream nixpkgs will use unstableGitUpdater or something
+    nix hash convert --to sri --hash-algo sha256 "$(nix-prefetch-url --unpack "$(nix eval --raw .#wayback-x11.src.url)")"
+*/
+    hash = "sha256-IfEdN5D68+df4RMASJO6GHsF4blAmEPRHTF56FHBrz4=";
   };
 
   nativeBuildInputs = [
@@ -49,7 +53,7 @@ stdenv.mkDerivation {
 
   meta = {
     description = "X11 compatibility layer leveraging wlroots and Xwayland";
-    homepage = "https://gitlab.freedesktop.org/wayback/wayback";
+    homepage = "https://wayback.freedesktop.org";
     license = lib.licenses.mit;
     mainProgram = "wayback-session";
   };
