76c3dc6488b0c3ee912062bf7db224998cf15bf1
dramforever <dramforever@live.com>
2025-08-11 23:13:28 +0800

    Override libkrun

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -32,6 +32,25 @@ self: super:
       };
     });
 
+  libkrun = super.libkrun.override {
+    virglrenderer =
+      assert super.virglrenderer.version == "1.1.1";
+      super.virglrenderer.overrideAttrs (old: {
+        version = "1.1.1-unstable-2025-08-11";
+        src = self.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
+          owner = "virgl";
+          repo = "virglrenderer";
+          rev = "423c6f3fe687093e1327ffd846cff9668c492b1d";
+          hash = "sha256-NkAJ6Pk5+cAyIhK+Dx2THiuzql3JntMNNEuDRK1SWaw=";
+        };
+
+        mesonFlags = old.mesonFlags ++ [
+          (self.lib.mesonOption "drm-renderers" "amdgpu-experimental,msm,asahi")
+        ];
+      });
+  };
+
   nixos-option = super.nixos-option.override {
     nix = self.nix-dram;
   };
