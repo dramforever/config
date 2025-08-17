8c1a6e5f9e1af241e6a9e08cdaae6c2dd4ae670f
dramforever <dramforever@live.com>
2025-08-17 19:24:48 +0800

    homura: Use attrset for availableKernelModules

--- homura/boot.nix
+++ homura/boot.nix
@@ -19,47 +19,11 @@
     };
 
     kernelPackages = lib.mkForce pkgs.linuxPackages_asahi;
-    initrd.availableKernelModules = lib.mkForce [
-      # BTRFS
-      "crc32c"
-      "xxhash_generic"
-      "blake2b_generic"
-      "sha256_generic"
-
-      # Asahi Linux
-      "apple-mailbox"
-      "nvme_apple"
-      "pinctrl-apple-gpio"
-      "macsmc"
-      "macsmc-rtkit"
-      "i2c-pasemi-platform"
-      "tps6598x"
-      "apple-dart"
-      "dwc3"
-      "dwc3-of-simple"
-      "xhci-pci"
-      "pcie-apple"
-      "gpio_macsmc"
-      "phy-apple-atc"
-      "nvmem_apple_efuses"
-      "spi-apple"
-      "spi-hid-apple"
-      "spi-hid-apple-of"
-      "rtc-macsmc"
-      # "simple-mfd-spmi"
-      "spmi-apple-controller"
-      "apple_nvmem_spmi" # "nvmem_spmi_mfd"
-      "apple-dockchannel"
-      "dockchannel-hid"
-      "apple-rtkit-helper"
-
-      # additional stuff necessary to boot off USB for the installer
-      # and if the initrd (i.e. stage 1) goes wrong
-      "usb-storage"
-      "xhci-plat-hcd"
-      "usbhid"
-      "hid_generic"
-    ];
+    initrd.availableKernelModules = {
+      simple-mfd-spmi = lib.mkForce false;
+      nvmem_spmi_mfd = lib.mkForce false;
+      apple_nvmem_spmi = true;
+    };
   };
 
   boot.loader.systemd-boot.extraFiles."asahi-efi/m1n1/boot.bin" =
