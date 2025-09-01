c10aa5efd66b810ee0bdf78b35d7aa0da37f4441
dramforever <dramforever@live.com>
2025-09-01 08:30:18 +0800

    linuxPackages_asahi: 6.16.3-1 -> 6.16.4-1

--- bits/linux-asahi.nix
+++ bits/linux-asahi.nix
@@ -7,7 +7,7 @@
 
 buildLinux (args // rec {
   pname = "linux-asahi";
-  version = "6.16.3-1";
+  version = "6.16.4-1";
   modDirVersion = "${lib.head (lib.splitString "-" version)}-asahi";
   extraMeta.branch = "6.16";
 
@@ -21,34 +21,25 @@ buildLinux (args // rec {
         hash = "sha256-107ZKR5moEC0riw9L9SsANenZTnc6a9Aoha/kEHMHws=";
       };
     }
+    {
+      name = "linux-asahi-macaudio-default-m";
+      patch = ./linux-asahi-macaudio-default-m.diff;
+    }
   ] ++ args.kernelPatches;
 
   src = fetchFromGitHub {
     owner = "AsahiLinux";
     repo = "linux";
     rev = "asahi-${version}";
-    hash = "sha256-77DAAOfYwCdzcLxEnsxorUuwoxoVFM32x+Fg22LBG04=";
+    hash = "sha256-rpyErGc38CvIuXR2B8q8yz36FVR4gOTaSVxgQtWC71s=";
   };
 
   structuredExtraConfig  = with lib.kernel; {
     # For DRM_ASAHI
-    DRM = yes;
     ARM64_16K_PAGES = yes;
 
     # For perf
     APPLE_M1_CPU_PMU = yes;
-
-    # https://github.com/nix-community/nixos-apple-silicon/commit/93a4cc1e9cd4bbf97fe4c2a70cac35dc05d5ae8e
-    # SND_DMAENGINE_PCM = module;
-    SND_SOC_APPLE_MCA = module;
-    SND_SOC_APPLE_MACAUDIO = module;
-    SND_SOC_CS42L42_CORE = module;
-    SND_SOC_CS42L42 = module;
-    SND_SOC_CS42L83 = module;
-    SND_SOC_CS42L84 = module;
-    SND_SOC_TAS2764 = module;
-    SND_SOC_TAS2770 = module;
-    SND_SIMPLE_CARD_UTILS = module;
   };
 
   extraPassthru.updateScript = nix-update-script {
