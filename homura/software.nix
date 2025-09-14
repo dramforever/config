23820828b95e104de607f02c56111a8da9ced71f
dramforever <dramforever@live.com>
2025-09-14 14:59:52 +0800

    homura: Remove nixos-version

--- homura/software.nix
+++ homura/software.nix
@@ -41,4 +41,6 @@
     "${pkgs.kdePackages.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
 
   programs.command-not-found.enable = false;
+
+  system.tools.nixos-version.enable = false;
 }
