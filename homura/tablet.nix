479dd98afc0855a03d9e5c49b3ac88f1cf32b97a
dramforever <dramforever@live.com>
2025-10-07 16:24:36 +0800

    Update flake.lock

--- homura/tablet.nix
+++ homura/tablet.nix
@@ -3,9 +3,9 @@
 {
   services.xserver.wacom.enable = true;
 
-  services.udev.extraRules = ''
-    ACTION!="add", GOTO="tablet_end"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="256c", ATTR{idProduct} == "0064", RUN+="${lib.getExe pkgs.hid-bpf-uclogic} --device $sys$devpath"
-    LABEL="tablet_end"
-  '';
+  # services.udev.extraRules = ''
+  #   ACTION!="add", GOTO="tablet_end"
+  #   SUBSYSTEM=="usb", ATTR{idVendor}=="256c", ATTR{idProduct} == "0064", RUN+="${lib.getExe pkgs.hid-bpf-uclogic} --device $sys$devpath"
+  #   LABEL="tablet_end"
+  # '';
 }
