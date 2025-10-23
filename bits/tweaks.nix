38df232844a878b4d8774429ba2fcee427bc3a6e
dramforever <dramforever@live.com>
2025-10-23 15:33:56 +0800

    Update flake.lock

--- bits/tweaks.nix
+++ bits/tweaks.nix
@@ -14,6 +14,27 @@ self: super:
     ];
   });
 
+  qt6Packages = super.qt6Packages.overrideScope (kself: ksuper: {
+    fcitx5-qt =
+      assert ksuper.fcitx5-qt.patches == [];
+      ksuper.fcitx5-qt.overrideAttrs (old: {
+        patches = [
+          (self.fetchpatch2 {
+            url = "https://github.com/fcitx/fcitx5-qt/commit/46a07a85d191fd77a1efc39c8ed43d0cd87788d2.patch?full_index=1";
+            hash = "sha256-qv8Rj6YoFdMQLOB2R9LGgwCHKdhEji0Sg67W37jSIac=";
+          })
+          (self.fetchpatch2 {
+            url = "https://github.com/fcitx/fcitx5-qt/commit/6ac4fdd8e90ff9c25a5219e15e83740fa38c9c71.patch?full_index=1";
+            hash = "sha256-x0OdlIVmwVuq2TfBlgmfwaQszXLxwRFVf+gEU224uVA=";
+          })
+          (self.fetchpatch2 {
+            url = "https://github.com/fcitx/fcitx5-qt/commit/1d07f7e8d6a7ae8651eda658f87ab0c9df08bef4.patch?full_index=1";
+            hash = "sha256-22tKD7sbsTJcNqur9/Uf+XAvMvA7tzNQ9hUCMm+E+E0=";
+          })
+        ];
+      });
+  });
+
   m1n1 = super.m1n1.override {
     customLogo = ./nix-snowflake-asahi-colors-256.png;
   };
