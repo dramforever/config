98646a7c9a63597827bfb32ebaa5652100f96fb9
dramforever <dramforever@live.com>
2026-01-21 09:41:08 +0800

    sakuya: Don't unmanage my dock thing now

--- sakuya/net.nix
+++ sakuya/net.nix
@@ -17,8 +17,6 @@
   networking.firewall.allowedUDPPortRanges =
     [ { from = 1714; to = 1764; } ];
 
-  networking.networkmanager.unmanaged = [ "enp58s0u1u4" ];
-
   services.avahi.enable = true;
   services.avahi.nssmdns4 = true;
 }
