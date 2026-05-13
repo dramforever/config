5c505d9f6bace52c73f3e960cf21f4d261120225
dramforever <dramforever@live.com>
2026-05-13 16:23:45 +0800

    homura: Add aop-als-cal.bin

--- homura/hardware.nix
+++ homura/hardware.nix
@@ -11,4 +11,18 @@
       nix-store --add-fixed sha256 --recursive <path-to-asahi-esp>/asahi
     '';
   };
+
+  hardware.firmware = [
+    (pkgs.runCommand "firmware-aop-als-cal"
+      {
+        cal = pkgs.requireFile {
+          name = "aop-als-cal.bin";
+          hash = "sha256-vE7D+MueOLuweGVvKq5jt0KtrSADnqHCG6i//sTfLxg=";
+          message = "aop-als-cal.bin required";
+        };
+      }
+      ''
+        install -Dm0644 "$cal" $out/lib/firmware/apple/aop-als-cal.bin
+      '')
+  ];
 }
