da3f5cc3c45a27d38063559858db30f22585b1fc
dramforever <dramforever@live.com>
2026-05-15 18:17:30 +0800

    homura: Add __ptrace_may_access thing mitigation

--- homura/boot.nix
+++ homura/boot.nix
@@ -22,6 +22,7 @@
       "vm.swappiness" = 5;
       "vm.vfs_cache_pressure" = 50;
       "kernel.sysrq" = 1;
+      "kernel.yama.ptrace_scope" = 2;
     };
 
     kernelPackages = lib.mkForce pkgs.linuxPackages_asahi;
