7bad879a6269251319e5d51464a7634ad3038fb0
dramforever <dramforever@live.com>
2026-06-25 11:38:35 +0800

    common/mitigations: Remove rds mitigation

--- common/mitigations.nix
+++ common/mitigations.nix
@@ -1,8 +1,2 @@
 {
-  boot.blacklistedKernelModules = [ "rds" "rds_tcp" "rds_rdma" ];
-  boot.extraModprobeConfig = ''
-    install rds /run/current-system/sw/bin/false
-    install rds_tcp /run/current-system/sw/bin/false
-    install rds_rdma /run/current-system/sw/bin/false
-  '';
 }
