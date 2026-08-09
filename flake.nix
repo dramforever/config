45b026667cf6bc45205b79107f5affe93a0392dc
dramforever <dramforever@live.com>
2026-08-09 11:44:15 +0800

    Use nixexprs.tar.xz, update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Personal configuration and packages, by dramforever";
 
-  inputs.nixpkgs.url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
+  inputs.nixpkgs.url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.zst";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.simple-nixos-mailserver = {
     url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
