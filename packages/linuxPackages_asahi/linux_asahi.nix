ef0cca410775b755ffad984e48aff7733f86cf5c
dramforever <dramforever@live.com>
2026-08-29 16:11:37 +0800

    linuxPackages_asahi: 7.1.9-1 -> 7.1.10-1

--- packages/linuxPackages_asahi/linux_asahi.nix
+++ packages/linuxPackages_asahi/linux_asahi.nix
@@ -7,7 +7,7 @@
 
 buildLinux (args // rec {
   pname = "linux-asahi";
-  version = "7.1.9-1";
+  version = "7.1.10-1";
   modDirVersion = lib.head (lib.splitString "-" version);
 
   target = "vmlinuz.efi";
@@ -18,7 +18,7 @@ buildLinux (args // rec {
     owner = "AsahiLinux";
     repo = "linux";
     rev = "asahi-${version}";
-    hash = "sha256-5XxxWLbd7TN6qblIOwCzyk1zujbCAs06bXFwCxlzoX0=";
+    hash = "sha256-6NDRWwkPVabj+IwK4Sgg2Ot6hXANTccWLa2SLVTa+eA=";
   };
 
   structuredExtraConfig  = with lib.kernel; {
